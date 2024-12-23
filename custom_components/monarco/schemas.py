8c0907b9d956d541431fdec8e38f530cc20dbe9f
lupinglade <lupinglade@cyaneous.com>
2024-12-23 11:41:18 -0800

    fixed config

--- custom_components/monarco/schemas.py
+++ custom_components/monarco/schemas.py
@@ -46,17 +46,9 @@ CONFIG_SCHEMA = vol.Schema({
     vol.Required(CONF_SPI_CLKFREQ, default=DEFAULT_SPI_CLKFREQ): cv.positive_int,
     vol.Required(CONF_WATCHDOG_TIMEOUT, default=DEFAULT_WATCHDOG_TIMEOUT): cv.positive_int,
     vol.Required(CONF_AO1_NAME): cv.string,
-    CONF_AO1_DEVICE: selector({
-        "select": {
-            "options": [DeviceModel.NONE, DeviceModel.LUNOS_E2, DeviceModel.LUNOS_EGO]
-        }
-    }),
+    vol.Required(CONF_AO1_DEVICE): vol.All(str, vol.In([DeviceModel.NONE, DeviceModel.LUNOS_E2, DeviceModel.LUNOS_EGO])),
     vol.Required(CONF_AO2_NAME): cv.string,
-    CONF_AO2_DEVICE: selector({
-        "select": {
-            "options": [DeviceModel.NONE, DeviceModel.LUNOS_E2, DeviceModel.LUNOS_EGO]
-        }
-    })
+    vol.Required(CONF_AO2_DEVICE): vol.All(str, vol.In([DeviceModel.NONE, DeviceModel.LUNOS_E2, DeviceModel.LUNOS_EGO])),
     # "analog_output_1": section(
     #     AO1_SCHEMA,
     #     {"collapsed": False},
